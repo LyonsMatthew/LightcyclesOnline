17f9c423202f3e59d2fe5dd088f9c22ee1d256db
Matthew J. Lyons <lyonsmj@rose-hulman.edu>
2019-04-18 15:35:12 -0400

    Started making server socket

--- core/src/com/lightcycles/online/Server.java
+++ core/src/com/lightcycles/online/Server.java
@@ -1,6 +1,12 @@
 package com.lightcycles.online;
 
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.ServerSocketHints;
+
+import java.net.ServerSocket;
+
 public class Server
 {
-
+	ServerSocketHints sshints = new ServerSocketHints();
+//	ServerSocket ssocket = new ServerSocket(Net.Protocol.TCP, 17892, sshints);
 }
