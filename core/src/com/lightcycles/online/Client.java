53fce583fa94e8243b0a1859dff8b0120b8a7f69
Matthew J. Lyons <lyonsmj@rose-hulman.edu>
2019-04-18 18:52:15 -0400

    Clients and servers can connect now! Amazing!

--- core/src/com/lightcycles/online/Client.java
+++ core/src/com/lightcycles/online/Client.java
@@ -5,8 +5,26 @@ import com.badlogic.gdx.Net;
 import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.net.SocketHints;
 
+import java.io.IOException;
+
 public class Client
 {
-	SocketHints shints = new SocketHints();
-	Socket socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "localhost", 17892, shints);
+	boolean isPlayer;
+
+	SocketHints shints;
+	Socket socket;
+
+	public Client()
+	{
+		isPlayer = true;
+
+		shints = new SocketHints();
+		socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "localhost", 17892, shints);
+
+		try {
+			socket.getOutputStream().write("player".getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- core/src/com/lightcycles/online/Server.java
+++ core/src/com/lightcycles/online/Server.java
@@ -4,14 +4,50 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.net.ServerSocketHints;
 import com.badlogic.gdx.net.ServerSocket;
+import com.badlogic.gdx.net.Socket;
+import com.badlogic.gdx.net.SocketHints;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Server
 {
-	ServerSocketHints sshints = new ServerSocketHints();
-	ServerSocket ssocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 17892, sshints);
+	ServerSocketHints sshints;
+	ServerSocket ssocket;
+	ArrayList<Socket> clientPlayerSockets = new ArrayList<>();
+	ArrayList<BufferedReader> clientPlayerSocketInputStreams = new ArrayList<>();
+	ArrayList<Socket> clientBetterSockets = new ArrayList<>();
+	ArrayList<BufferedReader> clientBetterSocketInputStreams = new ArrayList<>();
 
 	public Server()
 	{
+		sshints = new ServerSocketHints(); //server socket properties
+		sshints.acceptTimeout = 0;
+		ssocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 17892, sshints);
 
+		SocketHints shints = new SocketHints(); //connecting client socket properties
+		shints.connectTimeout = 0;
+		shints.socketTimeout = 0;
+		while (true)
+		{
+			Socket clientSocket = ssocket.accept(shints);
+			BufferedReader buff = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			//check if it's a better or a player; for now, just assume all are players
+			try {
+				String playerOrBetter = buff.readLine();
+				if (playerOrBetter.equals("player")) {
+					clientPlayerSockets.add(clientSocket);
+					clientPlayerSocketInputStreams.add(buff);
+				} else {
+					clientBetterSockets.add(clientSocket);
+					clientBetterSocketInputStreams.add(buff);
+				}
+				System.out.println(playerOrBetter);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
